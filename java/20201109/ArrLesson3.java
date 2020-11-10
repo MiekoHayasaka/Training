682cb4048d2cf8996c6f3cf072aeaa88a28475b1
Mieko Hayasaka <hayasaka654@gmail.com>
2020-11-10 09:48:14 +0900

    ArrLesson3修正

--- java/20201109/ArrLesson3.java
+++ java/20201109/ArrLesson3.java
@@ -1,14 +1,21 @@
 import java.util.Arrays;
 public class ArrLesson3{
 	public static void main(String[] args){
-		int[] dataA = {3,5,10};
+		int[] dataA = {3,5,10}; //宣言と同時のみの書き方
+		double[] nums = {1.1,2.2,3.3};
+		boolean[] bools={false,false,true,true};
+		String[] strs={"boge","fuga","bar"};
 		int[] dataB = new int[] {4,6,10};
 		for(int i=0; i<dataA.length; i++){
 			System.out.println(dataA[i]);
 		}
-
+		//拡張for文(dataBから未読の要素が無くなるまでnに格納)
 		for(int n : dataB){
 			System.out.println(n);
+		}
+
+		for(String s : strs){
+			System.out.println(s);
 		}
 		System.out.println(Arrays.toString(dataA));
 	}
