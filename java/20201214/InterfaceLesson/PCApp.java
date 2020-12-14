b2c6b58038406a567b83b3eae53f9d7908417bed
Mieko Hayasaka <hayasaka654@gmail.com>
2020-12-14 14:42:08 +0900

    Exceptionクラス、Interfaceクラス作成

---
+++ java/20201214/InterfaceLesson/PCApp.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class PCApp{
+	public static void main(String[] args){
+		PC pc=new PC();
+		List<UsbConnectable> usbList=new ArrayList<>();
+		usbList.add(new Mouse());
+		usbList.add(new Memory());
+		usbList.add(new CardReader());
+		List<App> appList=new ArrayList<>();
+		appList.add(new Excel("エクセル"));
+		appList.add(new Word("ワード"));
+		appList.add(new PowerPoint("パワポ"));
+		Scanner sc = new Scanner(System.in);
+		while(true){
+			System.out.print("0.アプリ実行,1.USBデバイス接続,2.終わり>");
+			int select=sc.nextInt();
+			switch(select){
+			case 0:
+				pc.selectApp();
+				break;
+			case 1:
+				System.out.print("どのデバイスを接続しますか0.マウス、1.メモリー、2.カードリーダー＞");
+				int index=sc.nextInt();
+				UsbConnectable usb=usbList.get(index);
+				pc.setDevice(usb);
+				pc.usb();
+				break;
+			default:
+				System.out.println("アプリケーションを終了します。");
+				return;
+			}
+		}
+	}
+}
