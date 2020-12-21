f2a2411d6e8a2685e1a44b2b06789e7fce9536df
Mieko Hayasaka <hayasaka654@gmail.com>
2020-12-21 14:49:56 +0900

    servlet,jsp作成

---
+++ java/20201221/Lesson1/src/Lesson1/Lesson1.java
@@ -0,0 +1,19 @@
+package Lesson1;
+
+public class Lesson1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello!");
+		String str="Hello";
+		if(str.equalsIgnoreCase("hello")) {
+			System.out.println("ok");
+		}
+		try {
+			Thread.sleep(1000);
+		}catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("1秒経ちました");
+	}
+
+}
